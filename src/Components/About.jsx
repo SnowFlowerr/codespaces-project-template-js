a34a3ee1122e929cb3622264b367b0870c82e77f
SnowFlowerr <bhudeokrit@gmail.com>
2024-07-03 18:31:52 +0530

    package.json updated

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "red bricks with green grass abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Web Developer student studying at FunctionUp School Of Technology . I enjoy creating unique and simplistic user interfaces in creative ways.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I'm passionate about creating visually stunning designs that capture attention and tell compelling stories. With a background in graphic design and illustration, I specialize in bringing ideas to life through digital and print media.";
 
 const About = () => {
   return (
